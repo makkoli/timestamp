bcb008585282394393be7d314db43dc65d765fa7
Kyle <makkoli@vt.edu>
2016-10-11 06:20:56 +0000

    added some files

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:date', function (req, res) {
+  res.send('Hello World ' + req.params.date);
 });
+
+app.listen(8080);
